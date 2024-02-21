e58ccfd54f39e2e63a44876f546cf99b1e9226c2
Sohail1173 <mdsohailahmed711@gmail.com>
2024-02-21 12:07:26 +0530

    training pipelines updated

--- Xray/pipeline/training_pipeline.py
+++ Xray/pipeline/training_pipeline.py
@@ -11,5 +11,29 @@ class TrainPipeline:
 
         self.data_ingestion_config=DataIngestionConfig()
 
-        def start_data_ingestion(self) ->DataIngestionArtifact:
-            logging.info("Entered the start_data_ingestion method of ")
+        def start_data_ingestion(self) -> DataIngestionArtifact:
+            logging.info("Entered the start_data_ingestion method of TrainPipeline class")
+        try:
+
+            logging.info("Getting the data from s3 bucket")
+
+            data_ingestion = DataIngestion(
+                data_ingestion_config=self.data_ingestion_config,
+            )
+
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+            logging.info("Got the train_set and test_set from s3")
+
+            logging.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+            )
+
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise XrayException(e, sys)
+if __name__ == "__main__":
+    train_pipeline=TrainPipeline()
+    train_pipeline.start_data_ingestion()
+
