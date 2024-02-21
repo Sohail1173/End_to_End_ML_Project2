347121808acea548c5772fc255d87850a65394e6
Sohail1173 <mdsohailahmed711@gmail.com>
2024-02-21 12:55:23 +0530

    data ingestion

--- Xray/components/data_ingestion.py
+++ Xray/components/data_ingestion.py
@@ -1,7 +1,7 @@
 import sys
 
 from Xray.cloud_storage.s3_operation import S3operation
-from Xray.entity.artifact_entity import DataIngestionnArtifact
+from Xray.entity.artifact_entity import DataIngestionArtifact
 from Xray.entity.config_entity import DataInfgestionConfig
 from Xray.exception import XrayException
 from Xray.logger import logging
@@ -10,17 +10,48 @@ from Xray.logger import logging
 
 
 class DataIngestionn:
-    def __init__(self):
-        pass
+    def __init__(self,data_ingestion_config:DataInfgestionConfig):
+        self.data_ingestion_config=data_ingestion_config
+        self.s3=S3operation()
     def get_data_from_s3(self):
         try:
-            pass
+            logging.info("Entered the get_data_from_s3 method of data ingestion class")
+            self.s3.sync_folder_to_s3(
+                folder=self.data_ingestion_config.data_path,
+                bucket_name=self.data_ingestion_config.bucket_name,
+                bucket_folder_name=self.data_ingestion_config.s3_data_folder,
+            )
+
+            logging.info("Exited the get_data_from_s3 method od data ingestion class")
         except Exception as e:
             raise XrayException(e,sys)
-    def initiate_data_ingestion(self):
+        
+
+    def initiate_data_ingestion(self) -> DataIngestionArtifact:
+        logging.info(
+            "Entered the initiate_data_ingestion method of Data ingestion class"
+        )
+
         try:
-            pass
+            self.get_data_from_s3()
+
+            data_ingestion_artifact: DataIngestionArtifact = DataIngestionArtifact(
+                train_file_path=self.data_ingestion_config.train_data_path,
+                test_file_path=self.data_ingestion_config.test_data_path,
+            )
+
+            logging.info(
+                "Exited the initiate_data_ingestion method of Data ingestion class"
+            )
+
+            return data_ingestion_artifact
+
         except Exception as e:
-            raise XrayException(e,sys)
+            raise XrayException(e, sys)
+        
+if __name__ == "__main__":
+    train_pipeline=TrainPipeline()
+    train_pipeline.start_data_ingestion()
+
 
         
